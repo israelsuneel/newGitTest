0b597b92844db16db8a6fa280bdc3daa0f7a4fe2
israelsuneel <israelsuneel@gmail.com>
2016-04-21 17:50:36 +0530

    adding maven project

---
+++ com.learningautomation.selenium/src/test/java/MavenTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class MavenTest {
+
+	@Test
+	public void test_Method(){
+		System.out.println("hello");
+	}
+}
---
+++ com.learningautomation.selenium/src/test/java/com/learningautomation/selenium/MavenTest2.java
@@ -0,0 +1,10 @@
+package com.learningautomation.selenium;
+import org.testng.annotations.Test;
+
+public class MavenTest2 {
+
+	@Test
+	public void test_Method(){
+		System.out.println("hello");
+	}
+}
